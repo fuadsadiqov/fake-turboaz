3844ec1f680e22e1e9314863892924b67e42d799
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-08-25 23:32:48 +0400

    adding some change in front-end

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,14 +7,18 @@ import { HttpClientModule } from '@angular/common/http'
 import { ReactiveFormsModule } from '@angular/forms';
 import { CarManagementComponent } from './features/car-management/car-management.component';
 import { CarFormComponent } from './layout/sidebar/car-form/car-form.component';
-import { NotFoundComponent } from './views/not-found/not-found.component'
+import { NotFoundComponent } from './views/not-found/not-found.component';
+import { CarFilterComponent } from './features/car-filter/car-filter.component';
+import { HeaderComponent } from './layout/sidebar/header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     CarManagementComponent,
     CarFormComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    CarFilterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      'primary-red': '#ca1016',
+      'primary-green': '#7ed321;',
+      'white': '#FFFFFF'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+

fd6b37cd7668c59591a509221fc268933e6f7f08
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-08-28 14:50:14 +0400

    Adding image in back-end

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,6 @@
 const Express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const cors = require("cors");
-const multer = require("multer");
-// const Item = require('./models/model.js'); // Import the schema
-const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
 const itemRoutes = require('./routes/items');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -30,38 +26,6 @@ app.get('/api/cars/getCars', (request, response) => {
         response.send(result);
     });
 })
-
-// app.post('/api/cars/addCar', upload.single('image'), (request, response) => {
-//     database.collection('cars').count({}, function(error, numOfDocs) {      
-//       const carData = new Item ({
-//         id: (numOfDocs+1).toString(),
-//         category: request.body.category,
-//         attributes: request.body.attributes,
-//         year: request.body.year,
-//         maker: request.body.maker,
-//         model: request.body.model,
-//         color: request.body.color,
-//         power: request.body.power,
-//         petroleum: request.body.petroleum,
-//         mileage: request.body.mileage,
-//         gear: request.body.gear,
-//         newCar: request.body.newCar,
-//         price: request.body.price,
-//         additional: request.body.additional,
-//         seller: request.body.seller,
-//         engineVolume: request.body.engineVolume,
-//         transmission: request.body.transmission,
-//         image: request.file.name
-//       })
-//       database.collection('cars').insertOne(carData, function(error, result) {
-//         if (error) {
-//           console.error('Error inserting car:', error);
-//           return response.status(500).json({ error: 'Internal Server Error' });
-//         }
-//         response.status(201).json(`Cars added to database successfully`);
-//       });
-//     });
-// });
 
 app.use(bodyParser.json());
 app.use('/uploads', Express.static(path.join('uploads')));
